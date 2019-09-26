08e4e083ce9bd3574e0afa31f66c469b09b80e01
jordan hagood <jordan.hagood@yahoo.com>
2019-09-25 21:05:41 -0700

    liriSearch function that prompts user to select what they want to search. Whem movie is picked, it will run movieThis function, which console logs the data from the movie using axios to get omdb response

--- liri.js
+++ liri.js
@@ -1,11 +1,77 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var Spotify = require('node-spotify-api');
+// var Spotify = require('node-spotify-api');
 var axios = require("axios");
-var fs = require('fs');
+// var fs = require('fs');
 var inquirer = require('inquirer');
-var moment = require("moment");
+// var moment = require("moment");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+function movieThis(movie) {
+    var movieSelected = movie;
+    var queryURL = "http://www.omdbapi.com/?t=" + movieSelected + "&y=&plot=short&apikey=trilogy"
+    console.log(queryURL);
+    axios.get(queryURL).then(
+        function (response) {
+            if (response.data.Response === "True") {
+                console.log("\nTitle: " + response.data.Title + "\n");
+                console.log("Year Released: " + response.data.Year + "\n");
+                console.log("IMDB Rating: " + response.data.imdbRating + "\n");
+                console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n");
+                console.log("Country: " + response.data.Country + "\n");
+                console.log("Language: " + response.data.Language + "\n");
+                console.log("Plot: " + response.data.Plot + "\n");
+                console.log("Actors: " + response.data.Actors + "\n");
+            } else {
+                movieThis("Mr.+Nobody")
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+function liriSearch() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What do you want Liri to search?',
+                choices: ["movie", "concert", "song", "random"],
+                name: 'userchoice'
+            }
+        ])
+        .then(function (inquirerResponse) {
+            if (inquirerResponse.userchoice === "movie") {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'What movie do you want Liri to search?',
+                            name: 'moviechoice'
+                        }
+                    ])
+                    .then(function (inquirerResponse) {
+                        movieThis(inquirerResponse.moviechoice);
+                    });
+            }
+        });
+
+}
+
+liriSearch();
